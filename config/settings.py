e736d9924342ae92e3d522a4228c7c5d3a5fb5cb
andron-e <tut25674@temple.edu>
2025-10-20 23:05:25 -0400

    updated settings configuration to allow pictures in web portal

--- config/settings.py
+++ config/settings.py
@@ -20,6 +20,15 @@ INSTALLED_APPS = [
     "storages",           # <- for S3
 ]
 
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -35,7 +44,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 TEMPLATES = [{
     "BACKEND": "django.template.backends.django.DjangoTemplates",
-    "DIRS": [BASE_DIR / "images" / "templates"],  # your app templates
+    "DIRS": [BASE_DIR / "images" / "templates"],  
     "APP_DIRS": True,
     "OPTIONS": {"context_processors": [
         "django.template.context_processors.debug",
@@ -53,13 +62,13 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Media (we’ll serve from S3 in prod; local fallback below)
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-AWS_STORAGE_BUCKET_NAME = os.getenv("cis4517-image-app")
-AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME", "cis4517-image-app")
 AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME", "us-east-2")
-AWS_QUERYSTRING_AUTH = False  # cleaner public URLs for demo
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+AWS_QUERYSTRING_AUTH = True  # cleaner public URLs for demo
+AWS_QUERYSTRING_EXPIRE = 3600
 
 
-# Optional: if using keys locally (prefer IAM role on EC2)
+# if using keys locally (not using because IAM role on EC2)
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
